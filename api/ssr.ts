623196f6f25cee032e92d0ea18b07f0704a0aa49
govindgupta1912 <govindgupta7947@gmail.com>
2025-07-16 19:37:53 +0530

    five

---
+++ api/ssr.ts
@@ -0,0 +1,19 @@
+import { render } from "../dist/public/entry-server.js";
+
+export default function handler(req, res) {
+  const url = req.url;
+  const { html, head } = render(url);
+
+  res.setHeader("Content-Type", "text/html");
+  res.status(200).end(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        ${head}
+      </head>
+      <body>
+        <div id="root">${html}</div>
+      </body>
+    </html>
+  `);
+}
